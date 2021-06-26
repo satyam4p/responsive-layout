b5f40a1ab9e354d130d7be840e2bed6101b4adce
Prajakta Desai <prajakta@ngsupplychain.com>
2021-06-27 02:09:28 +0530

    initial

---
+++ test.js
@@ -0,0 +1,16 @@
+function solution(A) {
+
+    let sum = A.reduce((a,b)=>a+b,0);
+    let sorted = A.sort((a,b) => b-a);
+    let threshold = sum/2;
+    let filteredSum = Infinity;
+    let count = 0;
+    while(filteredSum >= threshold){
+        for(let i in A){
+            filteredSum = 
+        }
+    }
+    
+
+}
+solution([5,19,8,1]);
